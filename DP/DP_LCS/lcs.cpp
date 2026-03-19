d723805614339e483367d35f40a0dd512a7def65
KOUSHIK SARKAR <149875262+Koushik-23Sarkar@users.noreply.github.com>
2026-03-19 21:01:45 +0530

    Add LCS solution using dynamic programming

---
+++ DP/DP_LCS/lcs.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int dp[1001][1001];
+
+    int solve(int i, int j, string &text1, string &text2){
+
+        if(i < 0 || j < 0){
+            return 0;
+        }
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+
+        if(text1[i] == text2[j]){
+            return dp[i][j] = 1 + solve(i-1, j-1, text1, text2);
+        }
+
+        return dp[i][j] = max(
+            solve(i-1, j, text1, text2),
+            solve(i, j-1, text1, text2)
+        );
+    }
+
+    int longestCommonSubsequence(string text1, string text2) {
+        memset(dp, -1, sizeof(dp));
+        return solve(text1.size()-1, text2.size()-1, text1, text2);
+    }
+};
